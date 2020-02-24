d2e2b01c7ca36e14f779662dcbf4ee6aedefafb3
Zoltán Révész <zbrevesz@gmail.com>
2020-02-24 13:51:45 +0100

    Add ResponseList data class
    ResponseList data class will contain the cocktails the app gets through HTTP requests.
    Also: created some GET requests in RemoteServiceInterface and webservice with Retrofit in AppDatabase.

---
+++ app/src/main/java/com/example/cocktailapp/data/ResponseList.kt
@@ -0,0 +1,5 @@
+package com.example.cocktailapp.data
+
+data class ResponseList (
+    var cocktailList : List<Cocktail>
+)
--- app/src/main/java/com/example/cocktailapp/remote/RemoteServiceInterface.kt
+++ app/src/main/java/com/example/cocktailapp/remote/RemoteServiceInterface.kt
@@ -1,4 +1,17 @@
 package com.example.cocktailapp.remote
 
+import com.example.cocktailapp.data.Cocktail
+import com.example.cocktailapp.data.ResponseList
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+//Test API key is: 1
+
 interface RemoteServiceInterface {
+    @GET("search.php?s={strDrink}")
+    suspend fun getCocktails(@Path("strDrink") name: String, @Query("api_key") apikey: String): ResponseList
+
+    @GET("lookup.php?i={idDrink}")
+    suspend fun getCocktailById(@Path("idDrink") id: Int, @Query("api_key") apikey: String): Cocktail
 }
